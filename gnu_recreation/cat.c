e122398d9ece78c902a166fa940b0390d97da0cb
siamak a.m.o <iss.siamak@gmail.com>
2023-11-09 15:04:05 +0000

    clean up

--- gnu_recreation/cat.c
+++ gnu_recreation/cat.c
@@ -6,33 +6,34 @@
 
 #define BS 128*1024
 
-int main(int argc, char **argv)
+int
+main (int argc, char **argv)
 {
     int fd;
     size_t r;
     struct stat st;
 
 
-    if (stat(argv[1], &st))
+    if (stat (argv[1], &st))
     {
-        puts("ERROR.");
+        puts ("ERROR.");
         return 1;
     }
 
-    size_t block_size = st.st_blksize*8;
-    int pg_size = getpagesize();
+    size_t block_size = st.st_blksize * 8;
+    int pg_size = getpagesize ();
 
-//    printf("blksize=%ld, pagesize=%ld.\n", block_size, pg_size);
+//    printf ("blksize=%ld, pagesize=%ld.\n", block_size, pg_size);
 
 #if 0
-    char *buf = (char*)aligned_alloc(pg_size, block_size);
+    char *buf = (char*) aligned_alloc (pg_size, block_size);
 #else
-    char *buf = (char*)aligned_alloc(pg_size, BS);
+    char *buf = (char*) aligned_alloc (pg_size, BS);
 #endif
     fd = open (argv[1], 0);
 
     while ((r = read (fd, buf, block_size)) != 0)
-        write(1, buf, r);
+        write (1, buf, r);
 
-    free(buf);
+    free (buf);
 }
