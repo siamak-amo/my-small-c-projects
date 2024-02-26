8b5e8c638e95acc9dd02fc8066c14ac92c5d4dd7
siamak a.m.o <edu.siamak@gmail.com>
2024-02-26 21:40:08 +0330

    update doc, codem buffer should be zero initialized

--- codeM/codeM.c
+++ codeM/codeM.c
@@ -46,7 +46,7 @@
  *         size_t my_rand_fun (void) {...}
  *         int main(...)
  *         {
- *           char codem[CODEM_BUF_LEN];
+ *           char codem[CODEM_BUF_LEN] = {0};
  *           // if you need to use any of the *_rand_* functions
  *           codem_rand_init (my_rand_fun);
  *         }
