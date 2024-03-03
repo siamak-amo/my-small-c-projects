e85e86c8e876872f263349fde2239dad1bf1b8b7
siamak a.m.o <edu.siamak@gmail.com>
2024-03-03 16:17:42 +0330

    levenshtein distance - first commit

---
+++ leven.c
@@ -0,0 +1,14 @@
+/**
+ *  file: leven.c
+ *  created on: 3 Mar 2024
+ *
+ *  Implements Levenshtein Distance - single-file library
+ *
+ *  algorithm details(s1[.n], s2[.m]):
+ *     time: O()
+ *     memory: O( Min(n,m) )
+ *
+ *
+ *
+ *
+ **/
