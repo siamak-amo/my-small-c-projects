8ffd4cde8c03e12d8edaa611d5bff2105e9fd926
siamak a.m.o <edu.siamak@gmail.com>
2024-03-09 13:10:06 +0330

    print invalid command on more cases

--- codeM/codeM.c
+++ codeM/codeM.c
@@ -843,18 +843,19 @@ exec_command (char prev_comm, char comm, struct Opt *opt)
     case 'q':
       return 1;
 
-      /* comment */
     case '\n':
     case '\r':
     case '\\':
+    case ' ':
+      /* comments */
     case ';':
     case '#':
-    case ' ':
       return 0;
 
       /* invalid command */
     default:
-      if (prev_comm == '\n' || prev_comm == '\0' || opt->command_mode)
+      if (prev_comm == '\n' || prev_comm == '\0' || prev_comm == ' ' ||
+          prev_comm == '#'  || prev_comm == ';'  || opt->command_mode)
         fprintf (stderr, "Invalid command -- (%c)\n", comm);
     }
   return 0;
