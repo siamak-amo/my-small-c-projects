e25750e2a09feffca1f439c63378d23e15e01f6e
siamak a.m.o <edu.siamak@gmail.com>
2024-06-03 17:15:50 +0330

    use new signature of memnum

--- codeM/codeM_py.c
+++ codeM/codeM_py.c
@@ -235,7 +235,7 @@ py_rand_suffix (PyObject *self, PyObject *args)
   PyObject *result;
   char *result_ptr = py_mkstrbuf_H (result, CODEM_LEN, suffix);
 
-  codem_memnum (result_ptr);
+  codem_memnum (result_ptr, CODEM_LEN);
   codem_rands (result_ptr, offset);
 
   return result;
