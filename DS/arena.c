54a32727ea49bb1627019216da2cb34cb293aa27
siamak a.m.o <edu.siamak@gmail.com>
2024-06-26 17:59:51 +0330

    DS: update doc

--- DS/arena.c
+++ DS/arena.c
@@ -1,4 +1,4 @@
-/* This file is part of my-small-c-projects
+/* This file is part of my-small-c-projects <https://gitlab.com/SI.AMO/>
 
   This program is free software: you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by
