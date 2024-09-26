d32a4f79a489acf06617c28aff93640f9cfadd21
siamak a.m.o <edu.siamak@gmail.com>
2024-09-26 20:38:23 +0330

    Libs: add license

--- libs/unescape.h
+++ libs/unescape.h
@@ -1,3 +1,19 @@
+/* This file is part of my-small-c-projects <https://gitlab.com/SI.AMO/>
+
+  unescape.h is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
+
+  unescape.h is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  *  file: unescape.h
  *  created on: 26 Sep 2024
