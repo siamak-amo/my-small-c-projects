587ddca28b0e7dbc826cdd806019f34b81c28ceb
siamak a.m.o <edu.siamak@gmail.com>
2024-09-29 20:07:51 +0330

    Libs: move repeated code to a macro

--- libs/unescape.h
+++ libs/unescape.h
@@ -106,6 +106,71 @@ ssize_t unescape (char *buff);
 /* The same as `unescape` */
 ssize_t unescape2 (char *restrict dest, const char *restrict src);
 
+/* internal macros */
+#define nextw_b(val) {                          \
+    if (val) __next_eq (w_ptr, val);            \
+    else *w_ptr = val;                          \
+    break;                                      \
+  }
+
+#define __unscape(w_ptr, r_ptr)                 \
+  for ( ;; ++r_ptr) {                           \
+  if (*r_ptr == '\0') {                         \
+    *w_ptr = '\0';                              \
+    break;                                      \
+  }                                             \
+  if (*r_ptr != '\\') {                         \
+    __next_eq (w_ptr, *r_ptr);                  \
+    continue;                                   \
+  }                                             \
+  switch (*(++r_ptr)) {                         \
+  case '\0':                                    \
+  goto reterr; /* invalid string */             \
+  /* case of `\\x` --> `\\` and `x` */          \
+  case '\\': nextw_b ('\\');                    \
+  /* simple escapes */                          \
+  case 'a': nextw_b ('\a');                     \
+  case 'b': nextw_b ('\b');                     \
+  case 'e': nextw_b ('\e');                     \
+  case 't': nextw_b ('\t');                     \
+  case 'n': nextw_b ('\n');                     \
+  case 'v': nextw_b ('\v');                     \
+  case 'f': nextw_b ('\f');                     \
+  case 'r': nextw_b ('\r');                     \
+  case '\'': nextw_b ('\'');                    \
+  case '\"': nextw_b ('\"');                    \
+  /* advanced escapes */                        \
+  case 'x': /* HEX \xHH */                      \
+  {                                             \
+  unsigned char x = 0;                          \
+  if (*(++r_ptr) == 0)                          \
+    goto reterr;                                \
+  x += hchr2num (*r_ptr) * 16;                  \
+  if (*(++r_ptr) == 0)                          \
+    goto reterr;                                \
+  x += hchr2num (*r_ptr) * 1;                   \
+  nextw_b (x);                                  \
+  break;                                        \
+  }                                             \
+   case '0': /* Octal \0NNN */                  \
+   {                                            \
+     unsigned char x = 0;                       \
+     if (*(++r_ptr) == 0)                       \
+       goto reterr;                             \
+     x += nchr2num (*r_ptr) * 8 * 8;            \
+     if (*(++r_ptr) == 0)                       \
+       goto reterr;                             \
+     x += nchr2num (*r_ptr) * 1 * 8;            \
+     if (*(++r_ptr) == 0)                       \
+       goto reterr;                             \
+     x += nchr2num (*r_ptr) * 1 * 1;            \
+     nextw_b (x);                               \
+     break;                                     \
+   }                                            \
+   default:                                     \
+   goto reterr;                                 \
+   }                                            \
+}
 
 /* implementation */
 #ifdef UNESCAPE_IMPLEMENTATION
@@ -114,78 +179,8 @@ unescape2 (char *restrict dest, const char *restrict src)
 {
   char *w_ptr = dest;
   const char *r_ptr = src;
-  for ( ;; ++r_ptr)
-    {
-      if (*r_ptr == '\0')
-        {
-          *w_ptr = '\0';
-          break;
-        }
 
-      if (*r_ptr != '\\')
-        {
-          __next_eq (w_ptr, *r_ptr);
-          continue;
-        }
-
-      switch (*(++r_ptr))
-        {
-#define nextw_b(val) {                          \
-            if (val) __next_eq (w_ptr, val);    \
-            else *w_ptr = val;                  \
-            break;                              \
-          }
-        case '\0':
-          goto reterr; /* invalid string */
-
-          /* case of `\\x` --> `\\` and `x` */
-        case '\\': nextw_b ('\\');
-          /* simple escapes */
-        case 'a': nextw_b ('\a');
-        case 'b': nextw_b ('\b');
-        case 'e': nextw_b ('\e');
-        case 't': nextw_b ('\t');
-        case 'n': nextw_b ('\n');
-        case 'v': nextw_b ('\v');
-        case 'f': nextw_b ('\f');
-        case 'r': nextw_b ('\r');
-        case '\'': nextw_b ('\'');
-        case '\"': nextw_b ('\"');
-          /* advanced escapes */
-        case 'x': /* HEX \xHH */
-          {
-            unsigned char x = 0;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += hchr2num (*r_ptr) * 16;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += hchr2num (*r_ptr) * 1;
-
-            nextw_b (x);
-            break;
-          }
-        case '0': /* Octal \0NNN */
-          {
-            unsigned char x = 0;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += nchr2num (*r_ptr) * 8 * 8;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += nchr2num (*r_ptr) * 1 * 8;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += nchr2num (*r_ptr) * 1 * 1;
-
-            nextw_b (x);
-            break;
-          }
-
-        default:
-          goto reterr;
-        }
-    }
+  __unscape (w_ptr, r_ptr);
 
   /* success return */
   return (ssize_t)(w_ptr - dest);
@@ -200,79 +195,9 @@ unescape (char *buff)
 {
   char *w_ptr = buff;
   char *r_ptr = buff;
-  for ( ;; ++r_ptr)
-    {
-      if (*r_ptr == '\0')
-        {
-          *w_ptr = '\0';
-          break;
-        }
 
-      if (*r_ptr != '\\')
-        {
-          __next_eq (w_ptr, *r_ptr);
-          continue;
-        }
-
-      switch (*(++r_ptr))
-        {
-#define nextw_b(val) {                          \
-            if (val) __next_eq (w_ptr, val);    \
-            else *w_ptr = val;                  \
-            break;                              \
-          }
-        case '\0':
-          goto reterr; /* invalid string */
-
-          /* case of `\\x` --> `\\` and `x` */
-        case '\\': nextw_b ('\\');
-          /* simple escapes */
-        case 'a': nextw_b ('\a');
-        case 'b': nextw_b ('\b');
-        case 'e': nextw_b ('\e');
-        case 't': nextw_b ('\t');
-        case 'n': nextw_b ('\n');
-        case 'v': nextw_b ('\v');
-        case 'f': nextw_b ('\f');
-        case 'r': nextw_b ('\r');
-        case '\'': nextw_b ('\'');
-        case '\"': nextw_b ('\"');
-          /* advanced escapes */
-        case 'x': /* HEX \xHH */
-          {
-            unsigned char x = 0;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += hchr2num (*r_ptr) * 16;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += hchr2num (*r_ptr) * 1;
-
-            nextw_b (x);
-            break;
-          }
-        case '0': /* Octal \0NNN */
-          {
-            unsigned char x = 0;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += nchr2num (*r_ptr) * 8 * 8;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += nchr2num (*r_ptr) * 1 * 8;
-            if (*(++r_ptr) == 0)
-              goto reterr;
-            x += nchr2num (*r_ptr) * 1 * 1;
-
-            nextw_b (x);
-            break;
-
-        default:
-          goto reterr;
-          }
-        }
-    }
-
+  __unscape (w_ptr, r_ptr);
+  
   /* success return */
   return (ssize_t)(w_ptr - buff);
   /* failure return */
