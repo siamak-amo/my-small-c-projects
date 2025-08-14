ecb1b008e0019c0b7eba9f6dddaed5237fdfa1d9
siamak a.m.o <edu.siamak@gmail.com>
2025-08-14 12:41:05 +0330

    Libs: implement ANSI color support

--- libs/clistd.h
+++ libs/clistd.h
@@ -196,4 +196,78 @@ parse_std_options_only (int argc, char **argv,
 
 #endif /* CLI_IMPLEMENTATION */
 
+
+/**
+ *  ANSI color support
+ *
+ *  Usage:
+ *  1. Using the `$` macro in printf format parameter:
+ *     `$` macro, create strings with appropriate color code
+ *     which will get concatenated by the compiler:
+ *       $("Test: %s", F_RED())  generates:
+ *       "[RED FG COLOR CODE]"  "Test: %s"  "[RESET COLOR]"
+ *
+ *  2. Using COLOR_FMT and COLOR_ARG macros:
+ *     COLOR_FMT("(%d)")  create  "%s(%d)%s"  and,
+ *     COLOR_ARG(123, B_RED())  generates 3 arguments:
+ *       "[RED BG COLOR CODE]",  123,  "[RESET COLOR]"
+ *
+ *  For background color, B_xxx() and for foreground color F_xxx()
+ *  macro can be used, these macros accept at most one argument:
+ *   F_U, F_B, F_HI, F_HBI  and  B_HI for background.
+ *
+ *  Example: these two printf calls, generate identical output
+ *  ```{c}
+ *  printf ($("%s",    B_RED())       " -- "
+ *          $("`%s`",  F_BLUE(F_U))   ".\n",
+ *          "Error", "file.txt");
+ *
+ *  printf (COLOR_FMT("%s")  " -- "  COLOR_FMT("`%s`")  ".\n",
+ *          COLOR_ARG("Error",     B_RED()),
+ *          COLOR_ARG("file.txt",  F_BLUE(F_U)));
+ *  ```
+ **/
+#define __ESC__  "\033["
+#define COLOR_RESET   __ESC__ "0m"
+
+#define $(fmt, ...) __VA_ARGS__ fmt COLOR_RESET
+
+#define COLOR_FMT(x) "%s" x "%s"
+#define COLOR_ARG(x, __COLOR__) __COLOR__, x, COLOR_RESET
+
+/* To be used as argument for F_xxx() and B_xxx() macros */
+#define F_U     "4;3"    // Underline
+#define F_B     "1;3"    // Bold
+#define F_HI    "0;9"    // High intensity
+#define F_BHI   "1;9"    // Bold high intensity
+#define B_HI    "2;10"   // High intensity background
+
+#define __FG__  "0;3"    // Regular foreground (Internal)
+#define __BG__  "4"      // Regular background (Internal)
+
+#define VA_FIRST(x, ...) x
+#define __RAW_COLOR__(code, ...) \
+  __ESC__ VA_FIRST(__VA_ARGS__) code "m"
+
+/* Foreground color definitions */
+#define F_BLACK(...)   __RAW_COLOR__ ("0", ##__VA_ARGS__, __FG__)
+#define F_RED(...)     __RAW_COLOR__ ("1", ##__VA_ARGS__, __FG__)
+#define F_GREEN(...)   __RAW_COLOR__ ("2", ##__VA_ARGS__, __FG__)
+#define F_YELLOW(...)  __RAW_COLOR__ ("3", ##__VA_ARGS__, __FG__)
+#define F_BLUE(...)    __RAW_COLOR__ ("4", ##__VA_ARGS__, __FG__)
+#define F_PURPLE(...)  __RAW_COLOR__ ("5", ##__VA_ARGS__, __FG__)
+#define F_CYAN(...)    __RAW_COLOR__ ("6", ##__VA_ARGS__, __FG__)
+#define F_WHITE(...)   __RAW_COLOR__ ("7", ##__VA_ARGS__, __FG__)
+
+/* Background color definitions */
+#define B_BLACK(...)   __RAW_COLOR__ ("0", ##__VA_ARGS__, __BG__)
+#define B_RED(...)     __RAW_COLOR__ ("1", ##__VA_ARGS__, __BG__)
+#define B_GREEN(...)   __RAW_COLOR__ ("2", ##__VA_ARGS__, __BG__)
+#define B_YELLOW(...)  __RAW_COLOR__ ("3", ##__VA_ARGS__, __BG__)
+#define B_BLUE(...)    __RAW_COLOR__ ("4", ##__VA_ARGS__, __BG__)
+#define B_PURPLE(...)  __RAW_COLOR__ ("5", ##__VA_ARGS__, __BG__)
+#define B_CYAN(...)    __RAW_COLOR__ ("6", ##__VA_ARGS__, __BG__)
+#define B_WHITE(...)   __RAW_COLOR__ ("7", ##__VA_ARGS__, __BG__)
+/* End of ANSI color support */
+
 #endif /* CLI_STD__H__ */
