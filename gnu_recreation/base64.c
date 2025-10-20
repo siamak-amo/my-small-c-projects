e6b864fc2fb74663cbc9a918536d3d89c81c5ec5
siamak a.m.o <edu.siamak@gmail.com>
2025-10-20 21:53:53 +0330

    GNU recreation: base64, implement line by line IO

--- gnu_recreation/base64.c
+++ gnu_recreation/base64.c
@@ -7,6 +7,8 @@
       base64 [OPTIONS]... [FILE]
     Options:
       -d, --decode       to decode
+      -l, --line         to read line by line, and
+                         possess each line separately
           --version      print the version
   
     Compilation:
@@ -30,18 +32,28 @@ enum Mode
     DECODE_MODE,
     ENCODE_MODE
   };
+enum IO_Mode
+  {
+    CIN_COUT = 0,  /* char in char out */
+    LIN_LOUT       /* line in line out */
+  };
+
 static enum Mode mode = ENCODE_MODE;
-static int infd = STDIN_FILENO; /* input file */
-static int ofd = STDOUT_FILENO; /* output file */
+static enum IO_Mode ioMode = CIN_COUT;
+
+FILE *inf, *outf;
 
 #define OPTCMP(s1, s2) \
   (s1 != NULL && s2 != NULL && 0 == strcmp (s1, s2))
+#define MIN(a,b) ((a<b) ? a : b)
+#define MAX(a,b) ((a>b) ? a : b)
 
 int
 main (int argc, char **argv)
 {
   int EoO = 0;
   set_program_name (*argv);
+  inf = stdin, outf = stdout;
 
   for (--argc, ++argv; argc != 0; --argc, ++argv)
     {
@@ -54,6 +66,8 @@ main (int argc, char **argv)
                 EoO = 1;
               else if (OPTCMP (*argv, "--decode"))
                 mode = DECODE_MODE;
+              else if (OPTCMP (*argv, "--line"))
+                ioMode = LIN_LOUT;
               else if (OPTCMP (*argv, "--version"))
                 goto VERSION_OPT;
               else
@@ -62,6 +76,9 @@ main (int argc, char **argv)
 
             case 'd':
               mode = DECODE_MODE;
+              break;
+            case 'l':
+              ioMode = LIN_LOUT;
               break;
 
             case 'v':
@@ -76,13 +93,13 @@ main (int argc, char **argv)
         }
       else
         {
-          if (infd == STDIN_FILENO)
+          if (inf == stdin)
             {
               FILE *f = fopen (*argv, "r");
               if (!f)
                 warnln ("%s: %s", *argv, strerror (errno));
               else
-                infd = fileno (f);
+                inf = f;
             }
           else
             {
@@ -91,17 +108,60 @@ main (int argc, char **argv)
         }
     }
 
-  int err = 0;
+  int err = 0, rw=0;
+  char *line = NULL; size_t line_cap = 0;
+#define TMP_CAP (MAX (B64_ENCODE_B, B64_DECODE_B) + 1)
+  char tmp[TMP_CAP];
   { /* main logic */  
     switch (mode)
       {
       case ENCODE_MODE:
-        b64_stream_encode (infd, ofd, &err);
-        write (ofd, "\n", 1);
+        if (CIN_COUT == ioMode)
+          {
+            b64_stream_encode (fileno (inf), fileno (outf), &err);
+            fprintf (outf, "\n");
+          }
+        if (LIN_LOUT == ioMode)
+          {
+            while ((rw = getline (&line, &line_cap, inf) - 1) >= 0)
+              {
+                for (char *line_ptr = line; rw > 0;
+                     rw -= B64_DECODE_B, line_ptr += B64_DECODE_B)
+                  {
+                    int input_len = MIN(rw, B64_DECODE_B);
+                    int n = b64_encode (line_ptr, input_len, tmp, TMP_CAP, &err);
+                    if (n > 0)
+                      fprintf (outf, "%.*s", n, tmp);
+                    else
+                      break;
+                  }
+                fprintf (outf, "\n");
+              }
+          }
         break;
 
       case DECODE_MODE:
-        b64_stream_decode (infd, ofd, &err);
+        if (CIN_COUT == ioMode)
+          {
+            b64_stream_decode (fileno (inf), fileno (outf), &err);
+          }
+        if (LIN_LOUT == ioMode)
+          {
+             while ((rw = getline (&line, &line_cap, inf) - 1) >= 0)
+              {
+                for (char *line_ptr = line; rw > 0;
+                     rw -= B64_ENCODE_B, line_ptr += B64_ENCODE_B)
+                  {
+                    int input_len = MIN(rw, B64_ENCODE_B);
+                    int n = b64_decode (line_ptr, input_len, tmp, TMP_CAP, &err);
+                    if (n > 0)
+                      fprintf (outf, "%.*s", n, tmp);
+                    else
+                      break;
+                  }
+                fprintf (outf, "\n");
+              }
+          }
         break;
       }
 
@@ -122,9 +182,9 @@ main (int argc, char **argv)
   }
   
  End_of_Main:
-  if (infd != STDIN_FILENO)
-    {
-      close (infd);
-    }
+  if (inf && inf != stdin)
+    fclose (inf);
+  if (outf && outf != stdout)
+    fclose (outf);
   return 0;
 }
